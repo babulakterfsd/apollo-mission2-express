f0908784478a37d9c5cc1001e99a8127b011c9dd
Babul Akter <babulakterfsd@gmail.com>
2023-11-16 14:41:24 +0600

    server created and streamed

--- event.js
+++ event.js
@@ -1,14 +1,37 @@
 const EventEmitter = require('events');
 const eventEmitter = new EventEmitter();
+const http = require('http');
+const server = http.createServer();
+const fs = require('fs');
 
-eventEmitter.on('tutorial', (num1, num2) => {
-    console.log(num1 + num2);
+// event
+// eventEmitter.on('tutorial', (num1, num2) => {
+//     console.log(num1 + num2);
+// });
+
+// eventEmitter.on('tutorial', () => {
+//     setTimeout(() => {
+//         console.log('tutorial event has occurred');
+//     }, 3000);
+// });
+
+// eventEmitter.emit('tutorial', 1, 2);
+
+//server
+server.on('request', (req, res) => {
+    if(req.url === '/' && req.method === 'GET') {
+        const readableStream = fs.createReadStream(process.cwd() + '/readblefile.txt');
+
+        readableStream.on('data', (buffer) => {
+           res.write(buffer);
+        });
+
+        readableStream.on('end', () => {
+            res.end();
+        });
+    }
 });
 
-eventEmitter.on('tutorial', () => {
-    setTimeout(() => {
-        console.log('tutorial event has occurred');
-    }, 3000);
+server.listen(5000, () => {
+    console.log('nodejs server is running');
 });
-
-eventEmitter.emit('tutorial', 1, 2);
