14c1ef8875728f189bdfcbf92cd7edd34a667d8e
Babul Akter <babulakterfsd@gmail.com>
2023-11-17 11:55:04 +0600

    json parser added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,18 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 export const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Full stack dev Babul');
+//parsers
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Its a GET request');
+});
+
+app.post('/', (req: Request, res: Response) => {
+  const data = req.body;
+  res.json(data);
 });
 
 export default app;
